51a3f1b95d23e5783a93d6cf05edd31ebc7f0522
denizsurmeli <denizsurmeli@icloud.com>
2023-05-27 09:56:10 +0300

    chore: move utility function to a different file

---
+++ utility.py
@@ -0,0 +1,9 @@
+def parse_ip_addr():
+    fmsg = os.popen("ip -o addr | awk '/inet/ {print $2, $3, $4}'").read()
+    ip_addr_list = fmsg.strip().split("\n")
+    ip_list = []
+    for line in ip_addr_list:
+        cur = line.strip().split(" ")
+        if cur[1]=="inet":
+            ip_list.append({"interface": cur[0], "ip": cur[2].split("/")[0]})
+    return ip_list
